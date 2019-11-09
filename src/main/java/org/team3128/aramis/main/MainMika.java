4a45eea5b7f99f9aa72919799b5b584776bd59e6
MikaO13 <mika.disneypins@gmail.com>
2019-11-08 16:25:44 -0800

    I programmed a drivetrain for aramis

---
+++ src/main/java/org/team3128/aramis/main/MainMika.java
@@ -0,0 +1,127 @@
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import org.team3128.athos.autonomous.*;
+import org.team3128.athos.util.PrebotDeepSpaceConstants;
+
+import org.team3128.common.NarwhalRobot;
+import org.team3128.common.drive.DriveCommandRunning;
+import org.team3128.common.drive.SRXTankDrive;
+import org.team3128.common.drive.SRXTankDrive.Wheelbase;
+import org.team3128.common.drive.calibrationutility.DriveCalibrationUtility;
+import org.team3128.common.hardware.limelight.Limelight;
+import org.team3128.common.hardware.navigation.Gyro;
+import org.team3128.common.hardware.navigation.NavX;
+import org.team3128.common.util.Constants;
+import org.team3128.common.util.units.Angle;
+import org.team3128.common.util.units.Length;
+import org.team3128.common.vision.CmdHorizontalOffsetFeedbackDrive;
+import org.team3128.gromit.util.DeepSpaceConstants;
+import org.team3128.common.util.Log;
+import org.team3128.common.util.RobotMath;
+import org.team3128.common.util.datatypes.PIDConstants;
+import org.team3128.common.narwhaldashboard.NarwhalDashboard;
+import org.team3128.common.listener.ListenerManager;
+import org.team3128.common.listener.controllers.ControllerExtreme3D;
+import org.team3128.common.listener.controltypes.Button;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+//start typing the stuff to make this a robot that isn't non-functional and bad and blank and boring and stuff thanks lol
+    // - Mason Holst, "Helpful Reminders", published November 2019
+
+public class MainMika extends NarwhalRobot {
+    public Joystick joystick;
+    public ListenerManager lm;
+    private DriveCommandRunning dcr;
+    public SRXTankDrive tankDrive;
+
+    public TalonSRX leftLeader;
+    public VictorSPX leftFollower;
+    public TalonSRX rightLeader;
+    public VictorSPX rightFollower;
+
+	@Override
+	protected void constructHardware()
+	{
+        joystick = new Joystick(0);
+		lm = new ListenerManager(joystick);
+        addListenerManager(lm);
+        
+        leftLeader = new TalonSRX(15);
+        leftFollower = new VictorSPX(16);
+
+        rightLeader = new TalonSRX(10);
+        rightFollower = new VictorSPX(11);
+        leftFollower.set(ControlMode.Follower, leftLeader.getDeviceID());
+        rightFollower.set(ControlMode.Follower, rightLeader.getDeviceID());
+        SRXTankDrive.initialize(leftLeader, rightLeader, 13.21*Length.in, 32.3*Length.in, 3700);
+        tankDrive = SRXTankDrive.getInstance();
+        dcr = new DriveCommandRunning();
+
+    }
+    
+    @Override
+    protected void constructAutoPrograms() {
+    }
+
+    @Override
+	protected void setupListeners() {
+        lm.nameControl(ControllerExtreme3D.TWIST, "MoveTurn");
+		lm.nameControl(ControllerExtreme3D.JOYY, "MoveForwards");
+		lm.nameControl(ControllerExtreme3D.THROTTLE, "Throttle");		
+
+        lm.addMultiListener(() -> {
+            if (!dcr.isRunning) {
+                tankDrive.arcadeDrive(
+                    -0.7 * RobotMath.thresh(lm.getAxis("MoveTurn"), 0.1),
+                    -1.0 * RobotMath.thresh(lm.getAxis("MoveForwards"), 0.1),
+                    -1.0 * lm.getAxis("Throttle"),
+                     true
+                );
+            }
+			
+        }, "MoveTurn", "MoveForwards", "Throttle");
+        lm.nameControl(new Button(3), "button3");
+        lm.addButtonDownListener("button3", () ->
+        {
+            // some thing that i want to do here
+        }
+        lm.addButtonUpListener("button3", () ->
+        {
+            // some thing that i want to do here
+        }
+    }
+
+    @Override
+    protected void teleopPeriodic() {
+    }
+
+    @Override
+    protected void updateDashboard() {
+
+    }
+
+
+    public static void main(String... args) {
+        RobotBase.startRobot(MainMika::new);
+    }
+}
